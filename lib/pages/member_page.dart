13faeff15f143f75782a756114eeb57b67802463
陈博宇 <terry.chen@cloudkeeper.cn>
2019-03-25 18:02:07 +0800

    上拉加载demo

--- lib/pages/member_page.dart
+++ lib/pages/member_page.dart
@@ -1,11 +1,76 @@
 import 'package:flutter/material.dart';
-class MemberPage extends StatelessWidget {
+import 'dart:async';
+
+import 'package:flutter_easyrefresh/easy_refresh.dart';
+import 'package:flutter_easyrefresh/bezier_circle_header.dart';
+import 'package:flutter_easyrefresh/bezier_bounce_footer.dart';
+
+/// 弹出圆圈样式页面
+class MemberPage extends StatefulWidget {
+  @override
+  _MemberPageState createState() => _MemberPageState();
+}
+
+class _MemberPageState extends State<MemberPage> {
+  List<String> addStr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  List<String> str = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  GlobalKey<EasyRefreshState> _easyRefreshKey =
+      new GlobalKey<EasyRefreshState>();
+  GlobalKey<RefreshHeaderState> _headerKey =
+      new GlobalKey<RefreshHeaderState>();
+  GlobalKey<RefreshFooterState> _footerKey =
+      new GlobalKey<RefreshFooterState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('MemberPage'),
+      appBar: AppBar(
+        title: Text("BezierCircle"),
       ),
+      body: Center(
+          child: new EasyRefresh(
+        key: _easyRefreshKey,
+        refreshHeader: BezierCircleHeader(
+          key: _headerKey,
+          color: Theme.of(context).scaffoldBackgroundColor,
+        ),
+        refreshFooter: BezierBounceFooter(
+          key: _footerKey,
+          color: Theme.of(context).scaffoldBackgroundColor,
+        ),
+        child: new ListView.builder(
+            //ListView的Item
+            itemCount: str.length,
+            itemBuilder: (BuildContext context, int index) {
+              return new Container(
+                  height: 70.0,
+                  child: Card(
+                    child: new Center(
+                      child: new Text(
+                        str[index],
+                        style: new TextStyle(fontSize: 18.0),
+                      ),
+                    ),
+                  ));
+            }),
+        onRefresh: () async {
+          await new Future.delayed(const Duration(seconds: 2), () {
+            setState(() {
+              str.clear();
+              str.addAll(addStr);
+            });
+          });
+        },
+        loadMore: () async {
+          await new Future.delayed(const Duration(seconds: 1), () {
+            if (str.length < 20) {
+              setState(() {
+                str.addAll(addStr);
+              });
+            }
+          });
+        },
+      )),
     );
   }
 }
