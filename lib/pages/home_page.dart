b9b296dec08feab1a9eb0b0e2c58196f1424c4fa
陈博宇 <terry.chen@cloudkeeper.cn>
2019-03-25 10:50:20 +0800

    首页商品分类

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import '../service/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../widgets/top_navigator.dart';
 class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
@@ -16,13 +17,18 @@ class _HomePageState extends State<HomePage> {
         future:getHomePageContent(),
         builder: (context,snapshot){
           if(snapshot.hasData){
-             var data=json.decode(snapshot.data.toString());
-             List<Map> swiperDataList = (data['data']['slides'] as List).cast(); // 顶部轮播组件数
-             return Column(
-               children: <Widget>[
-                   SwiperDiy(swiperDataList:swiperDataList ),   //页面顶部轮播组件
-               ],
-             );
+              var data=json.decode(snapshot.data.toString());
+              List<Map> swiperDataList = (data['data']['slides'] as List).cast(); // 顶部轮播组件数
+              List<Map> navigatorList = (data['data']['category'] as List).cast();//类别列表
+              if(navigatorList.length>10){
+                navigatorList.removeRange(10, navigatorList.length);
+              }
+              return Column(
+                  children: <Widget>[
+                    SwiperDiy(swiperDataList:swiperDataList ),   //页面顶部轮播组件
+                    TopNavigator(navigatorList: navigatorList)
+                  ],
+              );
           }else{
             return Center(
               child: Text('加载中'),
---
+++ lib/widgets/top_navigator.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+class TopNavigator extends StatelessWidget {
+  final List navigatorList;
+  TopNavigator({Key key, this.navigatorList}) : super(key: key);
+  Widget _gridViewItemUI(BuildContext context,item){
+    return InkWell(
+      onTap: (){
+        print('点击了导航');
+      },
+      child: Column(
+        children: <Widget>[
+          Image.network(item['image'],width: ScreenUtil().setWidth(95)),
+          Text(item['mallCategoryName'])
+        ],
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(320),
+      padding: EdgeInsets.all(3.0),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding: EdgeInsets.all(4.0),
+        children: navigatorList.map((item){
+          return _gridViewItemUI(context, item);
+        }).toList(),
+      ),
+    );
+  }
+}
